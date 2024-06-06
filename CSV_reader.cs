89949e3212b73d3134aafc59bb1bb9894d1ef92b
natalie-christine <161007805+natalie-christine@users.noreply.github.com>
2024-06-06 12:35:43 +0200

    Update CSV_reader.cs

--- CSV_reader.cs
+++ CSV_reader.cs
@@ -2,23 +2,18 @@ using System;
 using System.Collections.Generic;
 using System.Globalization;
 using System.IO;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace A01_CSV
 {
     public class CSV_reader
-
     {
         public CSV_reader()
         {
-
             string path = "C:\\Users\\nscho\\Documents\\InputFile.txt";
 
-            if (string.IsNullOrEmpty(path))
+            if (string.IsNullOrEmpty(path) || !File.Exists(path))
             {
-                Console.WriteLine("There is no file");
+                Console.WriteLine("File does not exist");
                 return;
             }
 
@@ -37,39 +32,61 @@ namespace A01_CSV
 
                 List<string> outputList = new List<string>();
 
-                int DateValuesLines = 23;
+                int DateValuesLines = 21; // Anzahl der Datumszeilen
                 int max = contentList.Count;
+
+                // Prozessiere die Datumszeilen
                 for (int i = 0; i < DateValuesLines; i++)
                 {
                     string[] contents = contentList[i].Split('|');
-                    if (contents.Length > 0)
+                    if (contents.Length == 3)
                     {
                         string input = contents[0];
                         DateTime date;
                         if (DateTime.TryParseExact(input, new string[] { "yyyy-MM-dd", "MM/dd/yyyy", "dd.MM.yyyy", "dd-MM-yyyy", "MM-dd-yyyy" }, CultureInfo.InvariantCulture, DateTimeStyles.None, out date))
                         {
                             contents[0] = date.ToString("dd.MM.yyyy");
-                            Console.WriteLine(contents[0]);
                         }
 
                         string newLine = string.Join("|", contents);
                         outputList.Add(newLine);
+                        Console.WriteLine(contents[0]);
                     }
                 }
 
                 for (int i = DateValuesLines; i < max; i++)
                 {
                     string[] contents = contentList[i].Split('|');
-                    if (contents.Length > 0)
+                    if (contents.Length == 3)
                     {
                         string input = contents[0].Trim();
                         decimal parsedValue;
 
-                        input = input.Replace(",", "").Replace(".", ",");
-                        if (decimal.TryParse(input, NumberStyles.Any, new CultureInfo("de-DE"), out parsedValue))
+                        if (decimal.TryParse(input, NumberStyles.Any, CultureInfo.InvariantCulture, out parsedValue))
                         {
+                            // Standardformatierung
+                            string format = "#,##0.######";
+                            CultureInfo culture = new CultureInfo("de-DE");
 
-                            contents[0] = parsedValue.ToString("#,##0.######", new CultureInfo("de-DE")); // "F4", "D" 
+                 
+                            switch (i )
+                            {
+                                case 24:
+                                    format = "#,##0.00"; // 1.000,00
+                                    break;
+                                case 25:
+                                    format = "0.00"; // 3,50
+                                    break;
+                                case 26:
+                                    format = "0.000"; // 0,004
+                                    break;
+                                case 28:
+                                    format = "#,##0.00"; // 5.000,00
+                                    break;
+                            }
+
+                            contents[0] = parsedValue.ToString(format, culture);
+
                             string newLine = string.Join("|", contents);
                             outputList.Add(newLine);
                             Console.WriteLine(contents[0]);
@@ -81,7 +98,7 @@ namespace A01_CSV
                     }
                 }
 
-                string outputPath = "C:\\Users\\nscho\\Documents\\A18_output.txt";
+                string outputPath = "C:\\Users\\nscho\\Documents\\A20_output.txt";
 
                 using (StreamWriter writer = new StreamWriter(outputPath))
                 {
@@ -97,7 +114,6 @@ namespace A01_CSV
             {
                 Console.WriteLine($"An error occurred: {ex.Message}");
             }
-
         }
     }
 }
